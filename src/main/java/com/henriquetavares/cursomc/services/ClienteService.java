9d59a9dcfb9008266639ff4eef6540424a47d162
Henrique Tavares <ihenrits@gmail.com>
2018-08-29 09:32:48 -0300

    Endpoint /clientes/{id} disponivel

---
+++ src/main/java/com/henriquetavares/cursomc/services/ClienteService.java
@@ -0,0 +1,23 @@
+package com.henriquetavares.cursomc.services;
+
+import com.henriquetavares.cursomc.domain.Cliente;
+import com.henriquetavares.cursomc.repositories.ClienteRepository;
+import com.henriquetavares.cursomc.services.exceptions.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class ClienteService {
+
+    @Autowired
+    private ClienteRepository repo;
+
+    public Cliente buscar(Integer id) {
+        Optional<Cliente> obj = repo.findById(id);
+        return obj.orElseThrow(() -> new ObjectNotFoundException(
+                "Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
+    }
+
+}
