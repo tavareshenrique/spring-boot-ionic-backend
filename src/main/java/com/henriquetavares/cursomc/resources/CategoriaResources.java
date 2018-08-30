4ef5e289b60523f8fcf6d78dcc1c1d940f69d4fd
Henrique Tavares <ihenrits@gmail.com>
2018-08-30 11:15:58 -0300

    Paginação com parametros opcionais na Requisição

--- src/main/java/com/henriquetavares/cursomc/resources/CategoriaResources.java
+++ src/main/java/com/henriquetavares/cursomc/resources/CategoriaResources.java
@@ -4,6 +4,7 @@ import com.henriquetavares.cursomc.domain.Categoria;
 import com.henriquetavares.cursomc.dto.CategoriaDTO;
 import com.henriquetavares.cursomc.services.CategoriaService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
@@ -55,6 +56,16 @@ public class CategoriaResources {
     public ResponseEntity<List<CategoriaDTO>> findAll() {
         List<Categoria> list = service.findAll();
         List<CategoriaDTO> listDTO = list.stream().map(obj -> new CategoriaDTO(obj)).collect(Collectors.toList());
+        return ResponseEntity.ok().body(listDTO);
+    }
+
+    @RequestMapping(value = "/page", method=RequestMethod.GET)
+    public ResponseEntity<Page<CategoriaDTO>> findPage( @RequestParam(value = "page", defaultValue = "0") Integer page,
+                                                        @RequestParam(value = "linesPerPage", defaultValue = "24") Integer linesPerPage,
+                                                        @RequestParam(value = "orderBy", defaultValue = "nome") String orderBy,
+                                                        @RequestParam(value = "direction", defaultValue = "ASC") String direction ) {
+        Page<Categoria> list = service.findPage(page, linesPerPage, orderBy, direction);
+        Page<CategoriaDTO> listDTO = list.map(obj -> new CategoriaDTO(obj));
         return ResponseEntity.ok().body(listDTO);
     }
 
--- src/main/java/com/henriquetavares/cursomc/services/CategoriaService.java
+++ src/main/java/com/henriquetavares/cursomc/services/CategoriaService.java
@@ -4,8 +4,12 @@ import com.henriquetavares.cursomc.domain.Categoria;
 import com.henriquetavares.cursomc.repositories.CategoriaRepository;
 import com.henriquetavares.cursomc.services.exceptions.DataIntegrityException;
 import com.henriquetavares.cursomc.services.exceptions.ObjectNotFoundException;
+import com.sun.javafx.scene.traversal.Direction;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -45,7 +49,11 @@ public class CategoriaService {
         } catch (DataIntegrityViolationException e) {
             throw new DataIntegrityException("Não é possível excluir uma categoria que possui produtos.");
         }
+    }
 
+    public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+        PageRequest pageRequest = PageRequest.of(page, linesPerPage, Sort.Direction.valueOf(direction), orderBy);
+        return repo.findAll(pageRequest);
     }
 
 }
