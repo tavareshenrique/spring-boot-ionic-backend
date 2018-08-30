1f0675542813e981006bb6bc72d0a5847c0b0212
Henrique Tavares <ihenrits@gmail.com>
2018-08-30 15:08:54 -0300

    PUT, DELETE e GET para Cliente

---
+++ src/main/java/com/henriquetavares/cursomc/dto/ClienteDTO.java
@@ -0,0 +1,53 @@
+package com.henriquetavares.cursomc.dto;
+
+import com.henriquetavares.cursomc.domain.Cliente;
+import org.hibernate.validator.constraints.Length;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import java.io.Serializable;
+
+public class ClienteDTO implements Serializable {
+
+    private Integer id;
+    @NotEmpty
+    @Length(min = 5, max = 120, message = "O tamanho deve ser entre 5 e 120 caracteres")
+    private String nome;
+    @NotEmpty
+    @Email(message = "Email inválido")
+    private String email;
+
+    public ClienteDTO() {
+
+    }
+
+    public ClienteDTO(Cliente obj) {
+        id = obj.getId();
+        nome = obj.getNome();
+        email = obj.getEmail();
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
--- src/main/java/com/henriquetavares/cursomc/services/ClienteService.java
+++ src/main/java/com/henriquetavares/cursomc/services/ClienteService.java
@@ -1,11 +1,18 @@
 package com.henriquetavares.cursomc.services;
 
 import com.henriquetavares.cursomc.domain.Cliente;
+import com.henriquetavares.cursomc.dto.ClienteDTO;
 import com.henriquetavares.cursomc.repositories.ClienteRepository;
+import com.henriquetavares.cursomc.services.exceptions.DataIntegrityException;
 import com.henriquetavares.cursomc.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -18,6 +25,39 @@ public class ClienteService {
         Optional<Cliente> obj = repo.findById(id);
         return obj.orElseThrow(() -> new ObjectNotFoundException(
                 "Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
+    }
+
+    public List<Cliente> findAll() {
+        return repo.findAll();
+    }
+
+    public Cliente update(Cliente obj) {
+        Cliente newObj = find(obj.getId());
+        updateData(newObj, obj);
+        return repo.save(newObj);
+    }
+
+    public void delete(Integer id) {
+        find(id);
+        try {
+            repo.deleteById(id);
+        } catch (DataIntegrityViolationException e) {
+            throw new DataIntegrityException("Não é possível excluir um Cliente.");
+        }
+    }
+
+    public Page<Cliente> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+        PageRequest pageRequest = PageRequest.of(page, linesPerPage, Sort.Direction.valueOf(direction), orderBy);
+        return repo.findAll(pageRequest);
+    }
+
+    public Cliente fromDTO(ClienteDTO objDTO) {
+        return new Cliente(objDTO.getId(), objDTO.getNome(), objDTO.getEmail(), null, null);
+    }
+
+    private void updateData(Cliente newObj, Cliente obj) {
+        newObj.setNome(obj.getNome());
+        newObj.setEmail(obj.getEmail());
     }
 
 }
