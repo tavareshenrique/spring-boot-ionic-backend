fe0537cb72069949233377e6191a8dc817a5f8ae
Henrique Tavares <ihenrits@gmail.com>
2018-09-05 15:05:47 -0300

    Class

--- src/main/java/com/henriquetavares/cursomc/domain/Pedido.java
+++ src/main/java/com/henriquetavares/cursomc/domain/Pedido.java
@@ -4,10 +4,9 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 
 import javax.persistence.*;
 import java.io.Serializable;
-import java.util.Date;
-import java.util.HashSet;
-import java.util.Objects;
-import java.util.Set;
+import java.text.NumberFormat;
+import java.text.SimpleDateFormat;
+import java.util.*;
 
 @Entity
 public class Pedido implements Serializable {
@@ -115,5 +114,22 @@ public class Pedido implements Serializable {
     @Override
     public int hashCode() {
         return Objects.hash(id);
+    }
+
+    @Override
+    public String toString() {
+        NumberFormat nf = NumberFormat.getCurrencyInstance(new Locale("pt", "BR"));
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+        StringBuilder builder = new StringBuilder();
+        builder.append("Pedido Número: ").append(getId());
+        builder.append(", Instante: ").append(sdf.format(getInstante()));
+        builder.append(", Cliente: ").append(getCliente().getNome());
+        builder.append(", Situação do Pagamento: ").append(getPagamento().getEstado().getDescricao());
+        builder.append("\nDetalhes:\n");
+        for (ItemPedido ip: getItens()) {
+            builder.append(ip.toString());
+        }
+        builder.append("Valor Total: ").append(nf.format(getValorTotal()));
+        return builder.toString();
     }
 }
