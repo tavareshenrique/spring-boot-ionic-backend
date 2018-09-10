822dc57b768ce832258a0c8e315acf8c604c5151
Henrique Tavares <ihenrits@gmail.com>
2018-09-10 14:21:06 -0300

    Corrigindo / Trocando GET por POST

--- src/main/java/com/henriquetavares/cursomc/config/SecurityConfig.java
+++ src/main/java/com/henriquetavares/cursomc/config/SecurityConfig.java
@@ -60,7 +60,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
         http.cors().and().csrf().disable();
         http.authorizeRequests()
-                .antMatchers(HttpMethod.GET, PUBLIC_MATCHERS_POST).permitAll()
+                .antMatchers(HttpMethod.POST, PUBLIC_MATCHERS_POST).permitAll()
                 .antMatchers(HttpMethod.GET, PUBLIC_MATCHERS_GET).permitAll()
                 .antMatchers(PUBLIC_MATCHERS).permitAll()
                 .anyRequest().authenticated();
